ae2b6328f50998d86d2e343dfc7e41fc32bbda30
jimmy <jimmyhd_19@hotmail.com>
2023-03-05 15:51:17 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'demo-migration-angular14-to-angular15';
+}
